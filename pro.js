2acf7a6311ed97ad4ae3d09eccdffeeb4aa37718
White-Knight11 <95540151+White-Knight11@users.noreply.github.com>
2022-01-23 23:48:05 +0530

    Create pro.js

---
+++ pro.js
@@ -0,0 +1,4 @@
+let btnClr = document.querySelector('button');
+let inputs = document.querySelectorAll('input');
+
+btnClr.addEventListener('click',() =>{ inputs.forEach( input => input value = '') ;});
